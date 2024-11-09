a1416e2cb29fae30cddd52fe684c5583bef9c0ff
SOHAG CHAKRABORTY <94746700+csesohag02@users.noreply.github.com>
2024-11-09 23:32:10 +0530

    Update cryptography_encryption_solution.py

--- cryptography_with_python/cryptography_encryption/cryptography_encryption_solution.py
+++ cryptography_with_python/cryptography_encryption/cryptography_encryption_solution.py
@@ -1,22 +1,32 @@
-alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-
-message = input("Enter your message:\n").upper()
-shift_number = int(input("Enter the shift number:\n"))
-
-def encrypt(p_message, p_shift_amount):
-  cipher_message = ""
-  for char in p_message:
-    if char in alphabet:
-        position = alphabet.index(char)
-        new_position = position + p_shift_amount
-        while new_position > 25:
-            new_position = new_position - 26
-        new_letter = alphabet[new_position]
-        cipher_message += new_letter
-    else:
-        cipher_message += char
-  return f"The encoded text is {cipher_message}"
+"""
+Created on Sat Nov  9 23:21:32 2024
+@author: csesohag02
+"""
 
 
-encrypted_message = encrypt(message, shift_number)
-print(encrypted_message)
+# list of the characters
+characters = [' ', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', '\\', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~']
+length = len(characters)
+
+
+# encrypt function
+def encrypt(p_text, p_shift):
+    """
+    encrypt(text, shift) -> encypted_text
+    Take the text and shift number; return the encrypted text
+    """
+    encrypt_text = ""
+    for char in p_text:
+        position = characters.index(char)
+        new_position = position + p_shift
+        if new_position > length-1:
+            new_position = new_position - length
+        new_char = characters[new_position]
+        encrypt_text += new_char
+    return encrypt_text
+
+
+message = input("Enter your message: ")
+shift = int(input("Enter the shift number: "))
+encrypt_message = encrypt(message, shift)
+print(encrypt_message)
